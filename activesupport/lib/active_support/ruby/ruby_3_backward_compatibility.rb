aad9efb60c35b11d71a809f09fec7c7209cccaba
Tobias Kraze <tobias.kraze@makandra.de>
2022-11-28 15:32:41 +0100

    ruby 3.1: saner loading of ruby3-backward-compatibility

--- activesupport/lib/active_support/ruby/ruby_3_backward_compatibility.rb
+++ activesupport/lib/active_support/ruby/ruby_3_backward_compatibility.rb
@@ -1,10 +1,10 @@
 if RUBY_VERSION >= '3'
-  begin
-    # require some libraries that will be autopatched by /all
-    require 'erb'
-    require 'psych'
-    require 'uri'
+  # require some libraries that will be autopatched by /all
+  require 'erb'
+  require 'psych'
+  require 'uri'
 
+  begin
     require 'ruby3_backward_compatibility/compatibility/all'
   rescue LoadError
     fail 'Please add the ruby3-backward-compatibility gem to your Gemfile.'
