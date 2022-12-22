2a38534db7320e0c183ac8d38fbb944ba0044fb3
Tobias Kraze <tobias.kraze@makandra.de>
2022-12-22 10:39:34 +0100

    update ruby3-backward-compatibility and enforce version ~> 1.0

--- activesupport/lib/active_support/ruby/ruby_3_backward_compatibility.rb
+++ activesupport/lib/active_support/ruby/ruby_3_backward_compatibility.rb
@@ -9,4 +9,9 @@ if RUBY_VERSION >= '3'
   rescue LoadError
     fail 'Please add the ruby3-backward-compatibility gem to your Gemfile.'
   end
+
+  requirement = '~> 1.0'
+  unless Gem::Requirement.new(requirement).satisfied_by?(Gem::Version.new(Ruby3BackwardCompatibility::VERSION))
+    fail "Please make sure the ruby3-backward-compatibility gem satisfies '#{requirement}'."
+  end
 end
