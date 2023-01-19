32cc95018396769b25af370a83ffb0625f40399c
Tobias Kraze <tobias.kraze@makandra.de>
2023-01-19 17:04:47 +0100

    Backport fix for CVE-2023-22796

--- activesupport/lib/active_support/inflector.rb
+++ activesupport/lib/active_support/inflector.rb
@@ -206,7 +206,7 @@ module ActiveSupport
     #   "ActiveRecord::Errors".underscore # => active_record/errors
     def underscore(camel_cased_word)
       camel_cased_word.to_s.gsub(/::/, '/').
-        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([A-Z])([A-Z][a-z])/,'\1_\2').
         gsub(/([a-z\d])([A-Z])/,'\1_\2').
         tr("-", "_").
         downcase
