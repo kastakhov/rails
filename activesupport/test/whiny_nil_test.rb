433cb3f59be269330e592d6fe312afe4ef8be70b
Tobias Kraze <tobias.kraze@makandra.de>
2024-04-04 10:14:15 +0200

    Ruby 3.3: activesupport: changes due to different error messages

--- activesupport/test/whiny_nil_test.rb
+++ activesupport/test/whiny_nil_test.rb
@@ -13,38 +13,38 @@ class WhinyNilTest < Test::Unit::TestCase
   def test_unchanged
     nil.method_thats_not_in_whiners
   rescue NoMethodError => nme
-    assert(nme.message =~ /nil:NilClass/)
+    assert(nme.message =~ /for nil/)
   end
 
   def test_active_record
     nil.save!
   rescue NoMethodError => nme
-    assert(!(nme.message =~ /nil:NilClass/))
+    assert(!(nme.message =~ /for nil/))
     assert_match(/nil\.save!/, nme.message)
   end
 
   def test_array
     nil.each
   rescue NoMethodError => nme
-    assert(!(nme.message =~ /nil:NilClass/))
+    assert(!(nme.message =~ /for nil/))
     assert_match(/nil\.each/, nme.message)
   end
 
   def test_id
     nil.id
   rescue RuntimeError => nme
-    assert(!(nme.message =~ /nil:NilClass/))
+    assert((nme.message =~ /Called id for nil/))
   end
 
   def test_no_to_ary_coercion
     nil.to_ary
   rescue NoMethodError => nme
-    assert(nme.message =~ /nil:NilClass/)
+    assert(nme.message =~ /for nil/)
   end
 
   def test_no_to_str_coercion
     nil.to_str
   rescue NoMethodError => nme
-    assert(nme.message =~ /nil:NilClass/)
+    assert(nme.message =~ /for nil/)
   end
 end
